93f6b9cf453929905c7805493d7659342e9f4691
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-30 23:49:34 +0530

    Initial commit

---
+++ src/test/java/com/test/filemngt/SpringbootFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.filemngt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
